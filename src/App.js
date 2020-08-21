7fd69c8404047d1ae022edf618e5d5bbaf18ab2a
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-21 21:58:35 +0600

    pass dynamic data

--- src/App.js
+++ src/App.js
@@ -2,25 +2,37 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+  const Nayok = [ 'Rubel', 'Hero Alam', 'kasem', 'Jasim', 'Manna'];
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <p> I am a React Person</p>
+        <Person name={Nayok[0]} job="Herogiri"></Person>
+        <Person name={Nayok[1]} job="Dadagiri"></Person>
+
+      
       </header>
     </div>
   );
+}
+
+function Person(props){
+  console.log(props);
+  const personStyle = {
+    border: '2px solid tomato',
+    padding: '10px',
+    width: '400px',
+    margin:'10px',
+  }
+  return (
+    <div style={personStyle}>
+      <h1> Name: {props.name} </h1>
+      <h3> Job: {props.job} </h3>
+    </div>
+  )
 }
 
 export default App;
