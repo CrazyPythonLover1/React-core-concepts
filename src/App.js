d126acce47196cd21c2efa8e5cd73c5242d7867a
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-08-21 23:29:39 +0600

    pass dynamic object to component

--- src/App.js
+++ src/App.js
@@ -6,17 +6,49 @@ import './App.css';
 
 function App() {
   const Nayok = [ 'Rubel', 'Hero Alam', 'kasem', 'Jasim', 'Manna'];
+  const products = [
+    {name:'Photoshop', price:'$90.99'},
+    {name:'Illustrator', price:'$60.99'},
+    {name:'PDF Reader', price:'$6.99'},
+  ]
+  
   return (
     <div className="App">
       <header className="App-header">
        <p> I am a React Person</p>
+       <Product product={products[0]}>  </Product>
+       <Product product={products[1]}>  </Product>
+       <Product product={products[2]}>  </Product>
+        
         <Person name={Nayok[0]} job="Herogiri"></Person>
         <Person name={Nayok[1]} job="Dadagiri"></Person>
+        <Person name={Nayok[3]} job="Kamla"></Person>
 
       
       </header>
     </div>
   );
+}
+
+function Product(props){
+  const productStyle={
+    color: 'tomato',
+    border:'1px solid gray',
+    borderRadius: '10px',
+    backgroundColor: 'lightgray',
+    height: '200px',
+    width: '250px',
+    margin:'10px',
+    float: 'left',
+  }
+  const {name, price} = props.product
+  return (
+    <div style={productStyle}>
+      <h3>{name} </h3>
+      <h5> {price} </h5>
+      <button>Buy now</button>
+    </div>
+  )
 }
 
 function Person(props){
