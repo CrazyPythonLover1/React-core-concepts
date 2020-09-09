3d45f576ff55f0b9be796d6e95c218d2b029f48f
CrazyPythonLover1 <mainulislamfaruqi@gmail.com>
2020-09-09 17:02:45 +0600

    Firebase Authentication

--- src/App.js
+++ src/App.js
@@ -4,16 +4,79 @@ import './App.css';
 import Home from './components/Home/Home';
 import Header from './components/Header/Header';
 
+
+// import * as firebase from "firebase/app";
+// import "firebase/auth";
+// import firebaseConfig from './firebase.config';
+// 
+// firebase.initializeApp(firebaseConfig);
+
+
 export const CategoryContext = createContext();
 
 function App() {
   
   const [category, setCategory] = useState('Laptop');
 
-
+// sign in signout
+//     const [user, setUser] = useState({
+//     isSignedIn: false,
+//     name: '',
+//     email: '',
+//     photo: '',
+//    })
+// 
+//   const provider = new firebase.auth.GoogleAuthProvider();
+//   const handleSignIn  = () => {
+//     firebase.auth().signInWithPopup(provider)
+//     .then((result) => {
+//       var token = result.credential.accessToken;
+//       var {displayName,email,photoURL} = result.user;
+// 
+//       const signInUser = {
+//         isSignedIn: true,
+//         name: displayName,
+//         email: email,
+//         photo:photoURL
+//       }
+// 
+//       setUser(signInUser);
+//     })
+// 
+//   }
+// 
+//   const handleSignOut = () => {
+//     firebase.auth().signOut()
+//      .then((result) => {
+//       const signedOutUser = {
+//         isSignedIn: false,
+//         name: '',
+//         email: '',
+//         photo: '',
+//       }
+//       setUser(signedOutUser);
+//      })
+//      .catch((error) => {
+//        console.log(error)
+//      })
+//      
+//   }
   
   return (
     <CategoryContext.Provider value={[category, setCategory]} className="App">
+    
+//     {
+//         user.isSignedIn?<button onClick={ handleSignOut}> signUp  </button> :
+//          <button onClick={ handleSignIn}> Login  </button>
+//       }
+//       {
+//         user.isSignedIn && <div>
+//           <h3> Name: user.name</h3>
+//           <h5> Email: user.email</h5>
+//           <img src={user.photo} alt=" There is no img "/>
+//         </div>
+//       }
+//     
       <header className="App-header">
        <p> I am a React Person</p>
        <Header/>
